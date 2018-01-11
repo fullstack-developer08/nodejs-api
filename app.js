b9424cfb16264e171793a56ecdb9a837611a67d6
Arif Khan <tech.aarifkhan@gmail.com>
2018-01-11 19:41:18 +0530

    added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.get('', (req, res) => {
     res.send('test');
 })
-app.listen('https://sleepy-citadel-54178.herokuapp.com');
+app.listen(PORT);
 console.log('running');
